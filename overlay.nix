278d6a1370e71cc7ba6ce100e915c4f9d1b4a454
Stoyan Georgiev <93016278+st0y4n@users.noreply.github.com>
2026-03-29 12:19:59 +0300

    Update overlay.nix

--- overlay.nix
+++ overlay.nix
@@ -1,49 +1,47 @@
 final: prev: {
-    kdePackages = prev.kdePackages // {
-      plasma-workspace = let
+  kdePackages = prev.kdePackages.overrideScope (kdeFinal: kdePrev: {
+    plasma-workspace = let
+      # Use kdePrev to get the original package
+      basePkg = kdePrev.plasma-workspace;
 
-        # the package we want to override
-        basePkg = prev.kdePackages.plasma-workspace;
-
-        # a helper package that merges all the XDG_DATA_DIRS into a single directory
-        xdgdataPkg = pkgs.stdenv.mkDerivation {
-          name = "${basePkg.name}-xdgdata";
-          buildInputs = [ basePkg ];
-          dontUnpack = true;
-          dontFixup = true;
-          dontWrapQtApps = true;
-          installPhase = ''
-            mkdir -p $out/share
-            ( IFS=:
-              for DIR in $XDG_DATA_DIRS; do
-                if [[ -d "$DIR" ]]; then
-                  cp -r $DIR/. $out/share/
-                  chmod -R u+w $out/share
-                fi
-              done
-            )
-          '';
-        };
-
-        # undo the XDG_DATA_DIRS injection that is usually done in the qt wrapper
-        # script and instead inject the path of the above helper package
-        derivedPkg = basePkg.overrideAttrs {
-          preFixup = ''
-            for index in "''${!qtWrapperArgs[@]}"; do
-              if [[ ''${qtWrapperArgs[$((index+0))]} == "--prefix" ]] && [[ ''${qtWrapperArgs[$((index+1))]} == "XDG_DATA_DIRS" ]]; then
-                unset -v "qtWrapperArgs[$((index+0))]"
-                unset -v "qtWrapperArgs[$((index+1))]"
-                unset -v "qtWrapperArgs[$((index+2))]"
-                unset -v "qtWrapperArgs[$((index+3))]"
+      # Use final for the standard environment tools
+      xdgdataPkg = final.stdenv.mkDerivation {
+        name = "${basePkg.name}-xdgdata";
+        buildInputs = [ basePkg ];
+        dontUnpack = true;
+        dontFixup = true;
+        dontWrapQtApps = true;
+        installPhase = ''
+          mkdir -p $out/share
+          ( IFS=:
+            for DIR in $XDG_DATA_DIRS; do
+              if [[ -d "$DIR" ]]; then
+                cp -r $DIR/. $out/share/
+                chmod -R u+w $out/share
               fi
             done
-            qtWrapperArgs=("''${qtWrapperArgs[@]}")
-            qtWrapperArgs+=(--prefix XDG_DATA_DIRS : "${xdgdataPkg}/share")
-            qtWrapperArgs+=(--prefix XDG_DATA_DIRS : "$out/share")
-          '';
-        };
+          )
+        '';
+      };
 
-      in derivedPkg;
-    }
-  );
+      # Apply the attribute overrides
+      derivedPkg = basePkg.overrideAttrs (oldAttrs: {
+        preFixup = (oldAttrs.preFixup or "") + ''
+          for index in "''${!qtWrapperArgs[@]}"; do
+            if [[ "''${qtWrapperArgs[$index]}" == "--prefix" ]] && [[ "''${qtWrapperArgs[$((index+1))]}" == "XDG_DATA_DIRS" ]]; then
+              unset -v "qtWrapperArgs[$index]"
+              unset -v "qtWrapperArgs[$((index+1))]"
+              unset -v "qtWrapperArgs[$((index+2))]"
+              unset -v "qtWrapperArgs[$((index+3))]"
+            fi
+          done
+          # Re-index array after unsetting to prevent gaps
+          qtWrapperArgs=("''${qtWrapperArgs[@]}")
+          qtWrapperArgs+=(--prefix XDG_DATA_DIRS : "${xdgdataPkg}/share")
+          qtWrapperArgs+=(--prefix XDG_DATA_DIRS : "$out/share")
+        '';
+      });
+
+    in derivedPkg;
+  });
 }
